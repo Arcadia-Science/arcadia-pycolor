cdc4f6d7039875ace65df502727da31cc897aa8e
Dennis Sun <dennis.a.sun@gmail.com>
2023-01-23 23:39:46 +0000

    added setupy.py, __init__.py, and .gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Arcadia-Pycolor',
+    url='https://github.com/Arcadia-Science/arcadia-pycolor',
+    author='Dennis Sun',
+    author_email='dennis.sun@arcadiascience.com',
+    packages=['arcadia_pycolor'],
+    install_requires=['numpy', 'matplotlib', 'scipy'],
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
