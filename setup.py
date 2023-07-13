ffb21768f356ba6533f83cf42df8c478558801e4
Dennis Sun <dennis.a.sun@gmail.com>
2023-07-13 14:29:41 -0700

    added colorspacious as a dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Dennis Sun',
     author_email='dennis.sun@arcadiascience.com',
     packages=['arcadia_pycolor'],
-    install_requires=['numpy', 'matplotlib', 'plotly'],
+    install_requires=['numpy', 'matplotlib', 'plotly', 'colorspacious'],
     version='0.2',
     license='MIT',
     description="A Python package to distribute Arcadia's color and style guidelines for figures.",
