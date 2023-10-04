960b332c3c54ab080ea345385ff09adf9aa02ba5
Dennis Sun <dennis.a.sun@gmail.com>
2023-10-04 11:38:54 -0700

    incremented version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email='dennis.sun@arcadiascience.com',
     packages=['arcadia_pycolor'],
     install_requires=['numpy', 'matplotlib', 'plotly', 'colorspacious', 'ipython'],
-    version='0.3',
+    version='0.3.1',
     license='MIT',
     description="A Python package to distribute Arcadia's color and style guidelines for figures.",
     package_dir={
