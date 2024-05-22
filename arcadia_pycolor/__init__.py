15a0d4b9542be162e704190f7d108d40b32cc7d5
Dennis Sun <dennis.a.sun@gmail.com>
2024-05-22 09:52:27 -0700

    stage additional changes

--- arcadia_pycolor/__init__.py
+++ arcadia_pycolor/__init__.py
@@ -3,4 +3,6 @@ __all__ = ["classes", "colors", "display", "palettes"]
 from .classes import *
 from .colors import *
 from .display import *
+from .gradients import *
+from .mpl import *
 from .palettes import *
