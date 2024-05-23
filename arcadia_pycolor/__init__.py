aa83c5b32e1d9447c37a4e93bf070862cc0d15f5
Dennis Sun <dennis.a.sun@gmail.com>
2024-05-23 15:27:45 -0700

    fixed init imports

--- arcadia_pycolor/__init__.py
+++ arcadia_pycolor/__init__.py
@@ -1,13 +1,17 @@
+from arcadia_pycolor import classes, colors, gradients, mpl, palettes, plot
+
+from .classes import *
+from .colors import *
+
 __all__ = [
-    "classes",
-    "colors",
     "gradients",
     "mpl",
     "palettes",
     "plot",
 ]
 
-from arcadia_pycolor import gradients, mpl, palettes, plot
+classes_all = [name for name in dir(classes) if not name.startswith("_")]
+colors_all = [name for name in dir(colors) if not name.startswith("_")]
 
-from .classes import *
-from .colors import *
+__all__.extend(classes_all)
+__all__.extend(colors_all)
