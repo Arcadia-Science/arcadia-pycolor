04178e6e3327751534f24f02b47771e8a7f75336
Dennis Sun <dennis.a.sun@gmail.com>
2024-05-23 15:25:49 -0700

    change distribute_values to accept len of list instead of the list itself

--- arcadia_pycolor/utils.py
+++ arcadia_pycolor/utils.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 
-def distribute_values(objects, min_val=0, max_val=1):
-    n = len(objects)
-    if n <= 1:
-        return [(min_val - max_val) / 2] * n
-    return np.linspace(min_val, max_val, n).tolist()
+def distribute_values(num_points: int, min_val: float = 0.0, max_val: float = 1.0):
+    if num_points <= 1:
+        return [(min_val - max_val) / 2] * num_points
+    return np.linspace(min_val, max_val, num_points).tolist()
