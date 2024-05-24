d545d49920d2523d50be8bd47751e3d97764a12a
Dennis Sun <dennis.a.sun@gmail.com>
2024-05-24 11:51:07 -0700

    fix order of entries in distribute_values

--- arcadia_pycolor/utils.py
+++ arcadia_pycolor/utils.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def distribute_values(num_points: int, min_val: float = 0.0, max_val: float = 1.0):
     if num_points <= 1:
-        return [(min_val - max_val) / 2] * num_points
+        return [(max_val - min_val) / 2] * num_points
     return np.linspace(min_val, max_val, num_points).tolist()
