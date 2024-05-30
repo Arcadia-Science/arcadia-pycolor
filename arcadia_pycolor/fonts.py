31ab7cc3987ba11da1ea493c0420b2cf771471de
Dennis Sun <dennis.a.sun@gmail.com>
2024-05-30 16:25:32 -0700

    remove fonts module

--- arcadia_pycolor/fonts.py
+++
@@ -1,8 +0,0 @@
-import os
-
-_MONOSPACE_FONT = "Suisse Int'l Mono"
-
-_FONT_FOLDER = "~/Library/Fonts"
-_EXPANDED_FONT_FOLDER = os.path.expanduser(_FONT_FOLDER)
-
-_SUISSE_FONTS = [i for i in os.listdir(_EXPANDED_FONT_FOLDER) if "Suisse" in i]
