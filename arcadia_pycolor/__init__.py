6c8900b3bbc59a60902c1361beca038201c3baba
Keith Cheveralls <keith.chev@gmail.com>
2024-07-17 10:18:38 -0700

    remove hexcode from colors_all and remove star imports

--- arcadia_pycolor/__init__.py
+++ arcadia_pycolor/__init__.py
@@ -1,19 +1,22 @@
 from arcadia_pycolor import colors, cvd, gradients, mpl, palettes, plot, style_defaults
 
 from .colors import *
-from .gradient import *
-from .hexcode import *
-from .palette import *
+from .gradient import Gradient
+from .hexcode import HexCode
+from .palette import Palette
 
 __all__ = [
     "cvd",
+    "Gradient",
     "gradients",
+    "HexCode",
     "mpl",
+    "Palette",
     "palettes",
     "plot",
     "style_defaults",
 ]
 
-colors_all = [name for name in dir(colors) if not name.startswith("_")]
+colors_all = [name for name in dir(colors) if isinstance(getattr(colors, name), HexCode)]
 
 __all__.extend(colors_all)  # type: ignore
