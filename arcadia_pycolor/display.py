5221e54e5a6dc72796184ae6a9cfb05eb24a1f8c
Evan Kiefl <kiefl.evan@gmail.com>
2025-02-24 14:53:15 -0800

    No TYPE_CHECKING

--- arcadia_pycolor/display.py
+++ arcadia_pycolor/display.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from arcadia_pycolor.hexcode import HexCode
+from arcadia_pycolor.hexcode import HexCode
 
 
 def colorize(
