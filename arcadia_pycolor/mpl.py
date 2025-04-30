94035a7a5862d4f22e4abcbd4407e869e00ebc91
Wasim Sandhu <wasim.sandhu@arcadiascience.com>
2025-04-30 12:01:48 -0700

    fix: Docstring

--- arcadia_pycolor/mpl.py
+++ arcadia_pycolor/mpl.py
@@ -430,9 +430,9 @@ def style_plot(
         axes (Axes, optional): The matplotlib Axes to modify.
             If None, uses the most recent Axes.
         monospaced_axes (str, optional): Which axes to set to the default monospaced font.
-            Either 'x', 'y', 'both', 'all', or None.
+            Either 'x', 'y', 'both', 'all', or None ('both' and 'all' are equivalent).
         categorical_axes (str, optional): Which axes to set to categorical.
-            Either 'x', 'y', 'both', 'all', or None.
+            Either 'x', 'y', 'both', 'all', or None ('both' and 'all' are equivalent).
         colorbar_exists (bool): Whether a colorbar exists on the axis.
     """
 
